6bb4865cac720d445eddfdd7f3972154a8333bd8
Jenia Berestian <jberestian@gmail.com>
2022-09-14 16:17:28 +0300

    added fetch request in itemp-operation

--- src/redux/items/items-operations.js
+++ src/redux/items/items-operations.js
@@ -9,3 +9,48 @@ const isDublicate = ({ name }, items) => {
   });
   return Boolean(result);
 };
+
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetch',
+  async (_, thunkAPI) => {
+    try {
+      const data = await api.getContact();
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const addContact = createAsyncThunk(
+  'contacts/add',
+  async (data, { rejectWithValue }) => {
+    try {
+      const result = await api.addContact(data);
+      return result;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  },
+  {
+    condition: (data, { getState }) => {
+      const { contacts } = getState();
+      if (isDublicate(data, contacts.items)) {
+        alert(`${data.name} is alredy exist`);
+        return false;
+      }
+    },
+  }
+);
+
+export const removeContact = createAsyncThunk(
+  'contacts/remove',
+  async (id, { rejectWithValue }) => {
+    try {
+      await api.removeContact(id);
+      return id;
+    } catch (error) {
+      rejectWithValue(error);
+    }
+  }
+);
