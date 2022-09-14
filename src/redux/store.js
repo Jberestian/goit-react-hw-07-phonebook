265629e563369321d85b5c0fa1619f2729686074
Jenia Berestian <jberestian@gmail.com>
2022-09-14 11:25:32 +0300

    delete persist reducer from store.js and index.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,34 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { rootReducer } from './rootReducer';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-
-const contactPersistConfig = {
-  key: 'root',
-  storage,
-  whitelist: ['contacts'],
-};
-
-const persistedReducer = persistReducer(contactPersistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: persistedReducer,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  reducer: {},
 });
 
-export const persistor = persistStore(store);
 export default store;
