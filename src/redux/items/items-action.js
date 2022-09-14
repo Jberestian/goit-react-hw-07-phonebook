73b35a5779b5a1d16f472c5e2dc4f2da5bde7fe6
Jenia Berestian <jberestian@gmail.com>
2022-09-14 21:59:27 +0300

    fix some bugs

--- src/redux/items/items-action.js
+++ src/redux/items/items-action.js
@@ -1,25 +1,25 @@
-import { nanoid } from '@reduxjs/toolkit';
-import { createAction } from '@reduxjs/toolkit';
+// import { nanoid } from '@reduxjs/toolkit';
+// import { createAction } from '@reduxjs/toolkit';
 
-import axios from 'axios';
+// import axios from 'axios';
 
-export const addContact = createAction('contacts/add', (name, number) => {
-  return {
-    payload: { name, number, id: nanoid() },
-  };
-});
+// export const addContact = createAction('contacts/add', (name, number) => {
+//   return {
+//     payload: { name, number, id: nanoid() },
+//   };
+// });
 
-export const removeContact = createAction('contacts/remove');
+// export const removeContact = createAction('contacts/remove');
 
-export const fetchContacts = createAction('contacts/fetch', () => {
-  axios
-    .get('https://63218c7382f8687273b3f055.mockapi.io/contacts')
-    .then(({ data }) => {
-      return {
-        payload: {
-          data,
-        },
-      };
-    })
-    .catch(error => console.log(error.message));
-});
+// export const fetchContacts = createAction('contacts/fetch', () => {
+//   axios
+//     .get('https://63218c7382f8687273b3f055.mockapi.io/contacts')
+//     .then(({ data }) => {
+//       return {
+//         payload: {
+//           data,
+//         },
+//       };
+//     })
+//     .catch(error => console.log(error.message));
+// });
