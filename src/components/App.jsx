3972061affebc4cad8178553c98248a64bc7f195
Jenia Berestian <jberestian@gmail.com>
2022-09-15 10:10:33 +0300

    added functionality to app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,9 +12,18 @@ import ContactBook from './ContactBook/ContactBook';
 import Filter from './Filter/Filter';
 import Form from './Form/Form';
 
+import { getFilter } from 'redux/filter/filter-selector';
+import { getItems } from 'redux/items/items-selector';
+
 const App = () => {
-  const contacts = useSelector(store => store.contacts.contacts);
-  const filter = useSelector(store => store.contacts.filter);
+  const getState = ({ contacts }) => ({
+    loading: contacts.loading,
+    error: contacts.error,
+  });
+
+  const contacts = useSelector(getItems);
+  const filter = useSelector(getFilter);
+  const { loading, error } = useSelector(getState);
 
   const dispatch = useDispatch();
 
@@ -22,26 +31,20 @@ const App = () => {
     dispatch(fetchContacts());
   }, [dispatch]);
 
-  const onAddContacts = ({ name, number }) => {
-    const action = addContact(name, number);
-    const arrayOfName = contacts && contacts.map(contact => contact.name);
-    if (arrayOfName && arrayOfName.includes(name)) {
-      return alert(`${name} is already in contacts`);
-    }
+  const onAddContact = data => {
+    const action = addContact(data);
     dispatch(action);
   };
 
-  const onRemoveContacts = contact_id => {
-    dispatch(removeContact(contact_id));
+  const onRemoveContact = id => {
+    dispatch(removeContact(id));
   };
 
   const getFiltredContacts = () => {
-    const normalizeFilter = filter.toLowerCase();
-    const filterContacts =
-      contacts &&
-      contacts.filter(contact =>
-        contact.name.toLowerCase().includes(normalizeFilter)
-      );
+    const normalizeFilter = filter?.toLowerCase();
+    const filterContacts = contacts?.filter(contact =>
+      contact.name.toLowerCase().includes(normalizeFilter)
+    );
     return filterContacts;
   };
 
@@ -50,15 +53,69 @@ const App = () => {
   return (
     <div>
       <h1>Phonebook</h1>
-      <Form onSubmit={onAddContacts} />
+      <Form onSubmit={onAddContact} />
       <h2>Contacts</h2>
       <Filter
         value={filter}
         onChange={event => dispatch(setFilter(event.currentTarget.value))}
       />
-      <ContactBook contacts={filtredArray} removeContact={onRemoveContacts} />
+      {error && <p>Не удалось загрузить контакты!!! </p>}
+      {loading ? (
+        <p>...Loadind</p>
+      ) : (
+        <ContactBook contacts={filtredArray} removeContact={onRemoveContact} />
+      )}
     </div>
   );
 };
+
+// const App = () => {
+//   const contacts = useSelector(store => store.contacts.contacts);
+//   const filter = useSelector(store => store.contacts.filter);
+
+//   const dispatch = useDispatch();
+
+//   useEffect(() => {
+//     dispatch(fetchContacts());
+//   }, [dispatch]);
+
+//   const onAddContacts = ({ name, number }) => {
+//     const action = addContact(name, number);
+//     const arrayOfName = contacts && contacts.map(contact => contact.name);
+//     if (arrayOfName && arrayOfName.includes(name)) {
+//       return alert(`${name} is already in contacts`);
+//     }
+//     dispatch(action);
+//   };
+
+//   const onRemoveContacts = contact_id => {
+//     dispatch(removeContact(contact_id));
+//   };
+
+//   const getFiltredContacts = () => {
+//     const normalizeFilter = filter.toLowerCase();
+//     const filterContacts =
+//       contacts &&
+//       contacts.filter(contact =>
+//         contact.name.toLowerCase().includes(normalizeFilter)
+//       );
+//     return filterContacts;
+//   };
+
+//   const filtredArray = getFiltredContacts();
+
+//   return (
+//     <div>
+//       <h1>Phonebook</h1>
+//       <Form onSubmit={onAddContacts} />
+//       <h2>Contacts</h2>
+//       <Filter
+//         value={filter}
+//         onChange={event => dispatch(setFilter(event.currentTarget.value))}
+//       />
+//       <ContactBook contacts={filtredArray} removeContact={onRemoveContacts} />
+//     </div>
+//   );
+// };
 
 export default App;
--- src/redux/items/items-reducer.js
+++ src/redux/items/items-reducer.js
@@ -1,4 +1,5 @@
 import { createReducer } from '@reduxjs/toolkit';
+// import { createSlice } from '@reduxjs/toolkit';
 
 import { fetchContacts, addContact, removeContact } from './items-action';
 
@@ -8,54 +9,54 @@ const intitialStore = {
   error: null,
 };
 
-// export const contactsReducers = createReducer([], {
-//   [fetchContacts]: async store => {},
-//   [addContact]: (store, { payload }) => [...store, payload],
-//   [removeContact]: (store, { payload }) =>
-//     store.filter(item => item.id !== payload),
-// });
-
-export const contactsReducers = createReducer({
-  name: 'contacts',
-  intitialStore,
-  extraReducers: {
-    [fetchContacts.pending]: store => {
-      store.loading = true;
-      store.error = null;
-    },
-    [fetchContacts.fulfilled]: (store, { payload }) => {
-      store.loading = false;
-      store.items = payload;
-    },
-    [fetchContacts.rejected]: (store, { payload }) => {
-      store.loading = false;
-      store.error = payload;
-    },
-    [addContact.pending]: store => {
-      store.loading = true;
-      store.error = null;
-    },
-    [addContact.fulfilled]: (store, { payload }) => {
-      store.loading = false;
-      store.items.push(payload);
-    },
-    [addContact.rejected]: (store, { payload }) => {
-      store.loading = false;
-      store.error = payload;
-    },
-    [removeContact.pending]: store => {
-      store.loading = true;
-      store.error = null;
-    },
-    [removeContact.fulfilled]: (store, { payload }) => {
-      store.loading = false;
-      store.items = store.items.filter(item => item.id !== payload);
-    },
-    [removeContact.rejected]: (store, { payload }) => {
-      store.loading = false;
-      store.error = payload;
-    },
-  },
+export const contactsReducers = createReducer(intitialStore, {
+  [fetchContacts]: async store => {},
+  [addContact]: (store, { payload }) => [...store, payload],
+  [removeContact]: (store, { payload }) =>
+    store.filter(item => item.id !== payload),
 });
+
+// export const contactsReducers = createSlice({
+//   name: 'contacts',
+//   intitialStore,
+//   extraReducers: {
+//     [fetchContacts.pending]: store => {
+//       store.loading = true;
+//       store.error = null;
+//     },
+//     [fetchContacts.fulfilled]: (store, { payload }) => {
+//       store.loading = false;
+//       store.items = payload;
+//     },
+//     [fetchContacts.rejected]: (store, { payload }) => {
+//       store.loading = false;
+//       store.error = payload;
+//     },
+//     [addContact.pending]: store => {
+//       store.loading = true;
+//       store.error = null;
+//     },
+//     [addContact.fulfilled]: (store, { payload }) => {
+//       store.loading = false;
+//       store.items.push(payload);
+//     },
+//     [addContact.rejected]: (store, { payload }) => {
+//       store.loading = false;
+//       store.error = payload;
+//     },
+//     [removeContact.pending]: store => {
+//       store.loading = true;
+//       store.error = null;
+//     },
+//     [removeContact.fulfilled]: (store, { payload }) => {
+//       store.loading = false;
+//       store.items = store.items.filter(item => item.id !== payload);
+//     },
+//     [removeContact.rejected]: (store, { payload }) => {
+//       store.loading = false;
+//       store.error = payload;
+//     },
+//   },
+// });
 
 export default contactsReducers.reducer;
